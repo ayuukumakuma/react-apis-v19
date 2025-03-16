ee49047cef194cad217f6256d9d79054fa8ea32d
ayuukumakuma <ayuu.kumakuma@gmail.com>
2025-03-16 11:58:40 +0900

    Fix import path for BrowserRouter in main.tsx to align with updated routing library structure.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router";
 import { Routes } from "./routes";
 
 const rootElement = document.getElementById("root");
